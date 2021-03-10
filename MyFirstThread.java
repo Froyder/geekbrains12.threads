f099583e30f939a0dba14d2241316b45d6e293bd
Froyder <78364851+Froyder@users.noreply.github.com>
2021-03-10 22:26:56 +0300

    Update MyFirstThread.java
    Оформил синхронизацию потоков перед пересчетом итогового массива с помощью CyclicBarrier

--- MyFirstThread.java
+++ MyFirstThread.java
@@ -1,16 +1,27 @@
 package ru.geek.homeworks.lesson12.threads;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class MyFirstThread extends Thread {
     float[] array;
+    CyclicBarrier cb;
 
-    public MyFirstThread (float[] array) {
+    public MyFirstThread (float[] array, CyclicBarrier cb) {
         this.array = array;
+        this.cb = cb;
     }
 
     public void run() {
-        for (int i = 0; i < array.length; i++){
-            array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        try {
+            for (int i = 0; i < array.length; i++){
+                    array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            cb.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
         }
     }
-
 }
