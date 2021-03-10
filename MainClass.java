344e535fd870bad7a97b1a7babb26fadbabd2788
Froyder <78364851+Froyder@users.noreply.github.com>
2021-03-10 22:28:02 +0300

    Update MainClass.java
    Добавил CyclicBarrier (решает вопрос с синхронизацией).

--- MainClass.java
+++ MainClass.java
@@ -1,14 +1,26 @@
 package ru.geek.homeworks.lesson12.threads;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class MainClass {
 
     static final int SIZE = 10000000;
     static final int HALF = SIZE / 2;
+    CyclicBarrier cb = new CyclicBarrier(3);
 
     public static void main(String[] args) {
         MainClass example = new MainClass();
         new Thread(() -> example.method1()).start();
-        new Thread(() -> example.method2()).start();
+        new Thread(() -> {
+            try {
+                example.method2();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     //первый метод
@@ -33,7 +45,7 @@ public class MainClass {
     }
 
     //второй метод (с двумя потоками)
-    public void method2() {
+    public void method2() throws BrokenBarrierException, InterruptedException {
         float[] arr = new float[SIZE];
 
         //заполняем базовый массив единцами
@@ -47,19 +59,21 @@ public class MainClass {
 
         //создаем "половинки" массива, заполняем их
         float[] arr1 = new float[HALF];
-        System.arraycopy(arr, 0, arr1, 0, (arr.length/2));
+        System.arraycopy(arr, 0, arr1, 0, HALF);
         float[] arr2 = new float[HALF];
-        System.arraycopy(arr, (arr.length/2), arr2, 0, (arr.length/2));
+        System.arraycopy(arr, arr.length/2, arr2, 0, HALF);
 
         //в первом потоке пересчитываем по новой формуле arr1
-        new Thread(new MyFirstThread(arr1)).start();
+        new Thread(new MyFirstThread(arr1, cb)).start();
 
         //во втором потоке пересчитываем по новой формуле arr2
-        new Thread(new MyFirstThread(arr2)).start();
+        new Thread(new MyFirstThread(arr2, cb)).start();
+
+        cb.await();
 
         //"склеиваем" половинки в общий массив
         System.arraycopy(arr1, 0, arr, 0, arr1.length);
-        System.arraycopy(arr2, 0, arr, (arr.length/2), arr2.length);
+        System.arraycopy(arr2, 0, arr, arr.length/2, arr2.length);
 
         System.out.println("Второй массив пересчитан по новой формуле!");
 
